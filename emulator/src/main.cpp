6671728ef7fe326ff8ae886897b1183655d6045c
JM <jacobmontpetit@gmail.com>
2024-10-17 22:48:38 -0400

    emu:main: added rough ideas for emulator

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,7 +1,49 @@
 
 /*
  * Brief: emulator for CIS 655 instruction set HW2 project
- *
+ *        runs the binary program passed as argument
  */
 
-int main() { return 0; }
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+// example of possible instruction layout
+#pragma "packed"
+struct instruction {
+  uint16_t opcode : 8;
+  uint16_t reg1 : 4;
+  uint16_t reg2 : 4;
+};
+
+static_assert(sizeof(add) == sizeof(uint16_t),
+              "add instruction must be 16bits");
+
+struct Mem {
+  // Using vector because we will not know how much we will need
+  std::vector<uint8_t> program;
+  std::vector<uint8_t> data;
+
+  // size_t is max size of system pointer
+  // size_t will hold largest possible index
+  std::size_t program_counter;
+
+  // registers should be const size
+  std::array<uint16_t, 16> registers;
+};
+
+// need to pass in argv & argc
+int main() {
+  // should read in the binary program
+  // then CPU function should run on Mem
+  // user should be given a menu of options like
+  //
+  // s: step
+  // p: print: prints current instruction: current state of memory and registers
+  // d: dump: continuously steps and dumps step register and instruction
+  // mw: write value to memory args: address, value
+  // mr: read value at address: args: addess
+  return 0;
+}
