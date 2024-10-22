8708789cf928f2a22e99b0a3666a82c6bd34ce44
radioxeth <radiox@nad27.net>
2024-10-22 13:59:09 -0600

    some work on the alu - refactor opcode and improve so and maybe lo

--- emulator/src/alu.cpp
+++ emulator/src/alu.cpp
@@ -10,6 +10,25 @@ class ALU
     std::map<std::string, int> M = {
         {"M0", 0}, {"M1", 1}, {"M2", 2}, {"M3", 3}, {"M4", 4}, {"M5", 5}, {"M6", 6}, {"M7", 7}, {"M8", 8}, {"M9", 9}, {"M10", 10}, {"M11", 11}, {"M12", 12}, {"M13", 13}, {"M14", 14}, {"M15", 15}};
 
+    // build a dictionary of op codes
+
+    std::map<std::string, std::string> OPCODES = {
+        {"am", "0000"},
+        {"sm", "0001"},
+        {"mm", "0010"},
+        {"dm", "0011"},
+        {"bnem", "0100"},
+        {"sltm", "0101"},
+        {"ai", "0110"},
+        {"si", "0111"},
+        {"mi", "1000"},
+        {"di", "1001"},
+        {"lci", "1010"},
+        {"sci", "1011"},
+        {"lo", "1100"},
+        {"so", "1101"},
+        {"jb", "1110"}};
+
     int pc = 0; // Program counter
 
     // Array to hold function pointers, indexed by opcode
@@ -31,167 +50,169 @@ public:
         functions.resize(15);
 
         // Map opcodes to corresponding functions
-        functions[0] = [this](std::string op1, std::string op2, std::string dest)
-        { return am(op1, op2, dest); };
-        functions[1] = [this](std::string op1, std::string op2, std::string dest)
-        { return sm(op1, op2, dest); };
-        functions[2] = [this](std::string op1, std::string op2, std::string dest)
-        { return mm(op1, op2, dest); };
-        functions[3] = [this](std::string op1, std::string op2, std::string dest)
-        { return dm(op1, op2, dest); };
-        functions[4] = [this](std::string op1, std::string op2, std::string target)
-        { return bnem(op1, op2, target); };
-        functions[5] = [this](std::string op1, std::string op2, std::string dest)
-        { return sltm(op1, op2, dest); };
-        functions[6] = [this](std::string op1, std::string op2i, std::string dest)
-        { return ai(op1, op2i, dest); };
-        functions[7] = [this](std::string op1, std::string op2i, std::string dest)
-        { return si(op1, op2i, dest); };
-        functions[8] = [this](std::string op1, std::string op2i, std::string dest)
-        { return mi(op1, op2i, dest); };
-        functions[9] = [this](std::string op1, std::string op2i, std::string dest)
-        { return di(op1, op2i, dest); };
-        functions[10] = [this](std::string op1i, std::string op2i, std::string dest)
-        { return lci(op1i, op2i, dest); };
-        functions[11] = [this](std::string op1i, std::string op2i, std::string dest)
-        { return sci(op1i, op2i, dest); };
-        functions[12] = [this](std::string op1, std::string op2, std::string dest)
-        { return lo(op1, op2, dest); };
-        functions[13] = [this](std::string op1, std::string op2, std::string dest)
-        { return so(op1, op2, dest); };
-        functions[14] = [this](std::string j1i, std::string j2i, std::string j3i)
-        { return jb(j1i, j2i, j3i); };
+        functions[0] = [this](std::string op1, std::string op2, std::string op3)
+        { return am(op1, op2, op3); };
+        functions[1] = [this](std::string op1, std::string op2, std::string op3)
+        { return sm(op1, op2, op3); };
+        functions[2] = [this](std::string op1, std::string op2, std::string op3)
+        { return mm(op1, op2, op3); };
+        functions[3] = [this](std::string op1, std::string op2, std::string op3)
+        { return dm(op1, op2, op3); };
+        functions[4] = [this](std::string op1, std::string op2, std::string op3)
+        { return bnem(op1, op2, op3); };
+        functions[5] = [this](std::string op1, std::string op2, std::string op3)
+        { return sltm(op1, op2, op3); };
+        functions[6] = [this](std::string op1, std::string op2, std::string op3)
+        { return ai(op1, op2, op3); };
+        functions[7] = [this](std::string op1, std::string op2, std::string op3)
+        { return si(op1, op2, op3); };
+        functions[8] = [this](std::string op1, std::string op2, std::string op3)
+        { return mi(op1, op2, op3); };
+        functions[9] = [this](std::string op1, std::string op2, std::string op3)
+        { return di(op1, op2, op3); };
+        functions[10] = [this](std::string op1, std::string op2, std::string op3)
+        { return lci(op1, op2, op3); };
+        functions[11] = [this](std::string op1, std::string op2, std::string op3)
+        { return sci(op1, op2, op3); };
+        functions[12] = [this](std::string op1, std::string op2, std::string op3)
+        { return lo(op1, op2, op3); };
+        functions[13] = [this](std::string op1, std::string op2, std::string op3)
+        { return so(op1, op2, op3); };
+        functions[14] = [this](std::string op1, std::string op2, std::string op3)
+        { return jb(op1, op2, op3); };
     }
 
     // add memory-memory
     // opcode 0000
-    bool am(std::string op1, std::string op2, std::string destination)
+    bool am(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] + memory[M[op2]];
+        memory[M[op3]] = memory[M[op1]] + memory[M[op2]];
         return true;
     }
 
     // subtract memory-memory
     // opcode 0001
-    bool sm(std::string op1, std::string op2, std::string destination)
+    bool sm(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] - memory[M[op2]];
+        memory[M[op3]] = memory[M[op1]] - memory[M[op2]];
         return true;
     }
 
     // multiply memory-memory
     // opcode 0010
-    bool mm(std::string op1, std::string op2, std::string destination)
+    bool mm(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] * memory[M[op2]];
+        memory[M[op3]] = memory[M[op1]] * memory[M[op2]];
         return true;
     }
 
     // divide memory-memory
     // opcode 0011
-    bool dm(std::string op1, std::string op2, std::string destination)
+    bool dm(std::string op1, std::string op2, std::string op3)
     {
         if (memory[M[op2]] == 0)
         {
             return false;
         }
-        memory[M[destination]] = memory[M[op1]] / memory[M[op2]];
+        memory[M[op3]] = memory[M[op1]] / memory[M[op2]];
         return true;
     }
 
     // branch if not equal memory-memory
     // opcode 0100
-    bool bnem(std::string op1, std::string op2, std::string target)
+    bool bnem(std::string op1, std::string op2, std::string op3)
     {
         if (memory[M[op1]] != memory[M[op2]])
         {
-            pc += std::stoi(target); // Adjust program counter
+            pc += std::stoi(op3); // Adjust program counter
         }
         return true;
     }
 
     // set less than memory-memory
     // opcode 0101
-    bool sltm(std::string op1, std::string op2, std::string destination)
+    bool sltm(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = (memory[M[op1]] < memory[M[op2]]) ? 1 : 0;
+        memory[M[op3]] = (memory[M[op1]] < memory[M[op2]]) ? 1 : 0;
         return true;
     }
 
     // add immediate
     // opcode 0110
-    bool ai(std::string op1, std::string op2i, std::string destination)
+    bool ai(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] + std::stoi(op2i);
+        memory[M[op3]] = memory[M[op1]] + std::stoi(op2);
         return true;
     }
 
     // subtract immediate
     // opcode 0111
-    bool si(std::string op1, std::string op2i, std::string destination)
+    bool si(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] - std::stoi(op2i);
+        memory[M[op3]] = memory[M[op1]] - std::stoi(op2);
         return true;
     }
 
     // multiply immediate
     // opcode 1000
-    bool mi(std::string op1, std::string op2i, std::string destination)
+    bool mi(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] * std::stoi(op2i);
+        memory[M[op3]] = memory[M[op1]] * std::stoi(op2);
         return true;
     }
 
     // divide immediate
     // opcode 1001
-    bool di(std::string op1, std::string op2i, std::string destination)
+    bool di(std::string op1, std::string op2, std::string op3)
     {
-        if (std::stoi(op2i) == 0)
+        if (std::stoi(op2) == 0)
         {
             return false;
         }
-        memory[M[destination]] = memory[M[op1]] / std::stoi(op2i);
+        memory[M[op3]] = memory[M[op1]] / std::stoi(op2);
         return true;
     }
 
     // load constant immediate
     // opcode 1010
-    bool lci(std::string op1i, std::string op2i, std::string destination)
+    bool lci(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = std::stoi(op1i) + std::stoi(op2i);
+        memory[M[op3]] = std::stoi(op1) + std::stoi(op2);
         return true;
     }
 
     // subtract constant immediate
     // opcode 1011
-    bool sci(std::string op1i, std::string op2i, std::string destination)
+    bool sci(std::string op1, std::string op2, std::string op3)
     {
         // is this right?
-        memory[M[destination]] = std::stoi(op1i) + std::stoi(op2i);
+        memory[M[op3]] = std::stoi(op1) + std::stoi(op2);
         return true;
     }
 
     // load using offset
     // opcode 1100
-    bool lo(std::string op1, std::string op2, std::string destination)
+    bool lo(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[destination]] = memory[M[op1]] + memory[M[op2]];
+
+        memory[memory[M[op3]]] = M[op1] + memory[M[op2]];
+        // or memory[M[op3]] = memory[M[op1]] + memory[M[op2]];
         return true;
     }
 
     // store using offset
     // opcode 1101
-    bool so(std::string op1, std::string op2, std::string destination)
+    bool so(std::string op1, std::string op2, std::string op3)
     {
-        memory[M[op1]] = memory[M[op2]];
+        memory[memory[M[op1]] + memory[M[op1]]] = memory[M[op2]];
         return true;
     }
 
     // jump back
     // opcode 1110
-    bool jb(std::string j1i, std::string j2i, std::string j3i)
+    bool jb(std::string op1, std::string op2, std::string op3)
     {
-        pc -= std::stoi(j1i) + std::stoi(j2i) + std::stoi(j3i);
+        pc -= std::stoi(op1) + std::stoi(op2) + std::stoi(op3);
         return true;
     }
 
@@ -235,7 +256,8 @@ public:
     {
         // print opcode, op1, op2, op3
         std::cout << opcode << " " << op1 << " " << op2 << " " << op3 << std::endl;
-        int op = std::stoi(opcode, 0, 2);
+        std::string mappedOpCode = OPCODES[opcode];
+        int op = std::stoi(mappedOpCode, 0, 2);
         if (op >= 0 && op < functions.size())
         {
             if (functions[op](op1, op2, op3))
@@ -259,60 +281,63 @@ public:
 int main()
 {
 
-    const int memSize = 48;
+    const int memSize = 64;
     std::vector<int> mainMemory(memSize, 0);
 
     ALU alu(mainMemory, memSize);
 
     // Example: Execute am with opcode 0
-    alu.execute("1010", "0", "8", "M0");
-    alu.execute("1010", "0", "8", "M1");
-    alu.execute("1010", "0", "7", "M2");
-    alu.execute("1010", "0", "11", "M3");
-    alu.execute("1010", "0", "10", "M4");
-    alu.execute("1010", "0", "4", "M5");
-    alu.execute("1010", "0", "1", "M6");
-    alu.execute("1010", "0", "0", "M7");
-    alu.execute("1010", "0", "1", "M8");
-    alu.execute("1010", "0", "1", "M9");
+    alu.execute("lci", "0", "8", "M0");
+    alu.execute("lci", "0", "8", "M1");
+    alu.execute("lci", "0", "7", "M2");
+    alu.execute("lci", "0", "11", "M3");
+    alu.execute("lci", "0", "10", "M4");
+    alu.execute("lci", "0", "4", "M5");
+    alu.execute("lci", "0", "1", "M6");
+    alu.execute("lci", "0", "0", "M7");
+    alu.execute("lci", "0", "1", "M8");
+    alu.execute("lci", "0", "1", "M9");
 
-    alu.execute("0000", "M9", "M6", "M15");  // am operation (add mem-mem)
-    alu.execute("1100", "M15", "M7", "M10"); // lo operation (load using offset)
+    // am m9 m6 m15
+    alu.execute("am", "M9", "M6", "M15"); // am operation (add mem-mem)
+    // lo m15 m7 m0
+    alu.execute("lo", "M15", "M7", "M10"); // lo operation (load using offset)
+
     // sltm m10 m0 m11
-    alu.execute("0101", "M10", "M0", "M11"); // sltm operation (set less than mem-mem)
+    alu.execute("sltm", "M10", "M0", "M11"); // sltm operation (set less than mem-mem)
     // bnem m11 m8 2
-    alu.execute("0100", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
+    alu.execute("bnem", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
     // lo m9 m6 m0
-    alu.execute("1100", "M9", "M6", "M0"); // lo operation (load using offset)
+    alu.execute("lo", "M9", "M6", "M0"); // lo operation (load using offset)
 
     // sltm m1 m10 m11
-    alu.execute("0101", "M1", "M10", "M11"); // sltm operation (set less than mem-mem)
+    alu.execute("sltm", "M1", "M10", "M11"); // sltm operation (set less than mem-mem)
     // bnem m11 m8 2
-    alu.execute("0100", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
+    alu.execute("bnem", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
     // lo m9 m6 m1
-    alu.execute("1100", "M9", "M6", "M1"); // lo operation (load using offset)
+    alu.execute("lo", "M9", "M6", "M1"); // lo operation (load using offset)
 
     // ai m6 1 m6
-    alu.execute("0110", "M6", "1", "M6"); // ai operation (add immediate)
+    alu.execute("ai", "M6", "1", "M6"); // ai operation (add immediate)
     // sltm m6 m5 m11
-    alu.execute("0101", "M6", "M5", "M11"); // sltm operation (set less than mem-mem)
+    alu.execute("sltm", "M6", "M5", "M11"); // sltm operation (set less than mem-mem)
     // bnem m11 m8 2
-    alu.execute("0100", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
+    alu.execute("bnem", "M11", "M8", "2"); // bnem operation (branch if not equal mem-mem)
     // jb 0 0 11
-    alu.execute("1110", "0", "0", "11"); // jb operation (jump back)
+    alu.execute("jb", "0", "0", "11"); // jb operation (jump back)
 
     // lci 0 15 m12
-    alu.execute("1010", "0", "15", "M12"); // lci operation (load constant immediate)
+    alu.execute("lci", "0", "15", "M12"); // lci operation (load constant immediate)
     // lci 0 15 m13
-    alu.execute("1010", "0", "15", "M13"); // lci operation (load constant immediate)
+    alu.execute("lci", "0", "15", "M13"); // lci operation (load constant immediate)
     // ai m12 1 m12
-    alu.execute("0110", "M12", "1", "M12"); // ai operation (add immediate)
+    alu.execute("ai", "M12", "1", "M12"); // ai operation (add immediate)
     // ai m13 2 m13
-    alu.execute("0110", "M13", "2", "M13"); // ai operation (add immediate)
+    alu.execute("ai", "M13", "2", "M13"); // ai operation (add immediate)
     // so m12 m7 m0
-    alu.execute("1101", "M12", "M7", "M0"); // so operation (store using offset)
+    alu.execute("so", "M12", "M7", "M0"); // so operation (store using offset)
     // so m13 m7 m1
-    alu.execute("1101", "M13", "M7", "M1"); // so operation (store using offset)
+    alu.execute("so", "M13", "M7", "M1"); // so operation (store using offset)
 
     return 0;
 }
