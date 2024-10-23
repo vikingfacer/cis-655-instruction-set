ce854f5397236354cc7bcc374bb475e2673e8e56
radioxeth <radiox@nad27.net>
2024-10-22 20:12:19 -0600

    return pc increment

--- emulator/src/alu.cpp
+++ emulator/src/alu.cpp
@@ -29,10 +29,10 @@ class ALU
         {"so", "1101"},
         {"jb", "1110"}};
 
-    int pc = 0; // Program counter
+    int pc = 32; // Program counter
 
     // Array to hold function pointers, indexed by opcode
-    std::vector<std::function<bool(std::string, std::string, std::string)>> functions;
+    std::vector<std::function<int(std::string, std::string, std::string)>> functions;
 
     // array of integers to represent memory
     std::vector<int> &memory;
@@ -43,7 +43,6 @@ class ALU
 public:
     ALU(std::vector<int> &mem, int memSize) : memory(mem), memorySize(memSize)
     {
-
         // not sure if we need memSize
 
         // Initialize the function array with 15 opcodes
@@ -84,136 +83,136 @@ public:
 
     // add memory-memory
     // opcode 0000
-    bool am(std::string op1, std::string op2, std::string op3)
+    int am(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] + memory[M[op2]];
-        return true;
+        return 1;
     }
 
     // subtract memory-memory
     // opcode 0001
-    bool sm(std::string op1, std::string op2, std::string op3)
+    int sm(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] - memory[M[op2]];
-        return true;
+        return 1;
     }
 
     // multiply memory-memory
     // opcode 0010
-    bool mm(std::string op1, std::string op2, std::string op3)
+    int mm(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] * memory[M[op2]];
-        return true;
+        return 1;
     }
 
     // divide memory-memory
     // opcode 0011
-    bool dm(std::string op1, std::string op2, std::string op3)
+    int dm(std::string op1, std::string op2, std::string op3)
     {
         if (memory[M[op2]] == 0)
         {
-            return false;
+            return 0;
         }
         memory[M[op3]] = memory[M[op1]] / memory[M[op2]];
-        return true;
+        return 1;
     }
 
     // branch if not equal memory-memory
     // opcode 0100
-    bool bnem(std::string op1, std::string op2, std::string op3)
+    int bnem(std::string op1, std::string op2, std::string op3)
     {
         if (memory[M[op1]] != memory[M[op2]])
         {
-            pc += std::stoi(op3); // Adjust program counter
+            return std::stoi(op3); // Adjust program counter
         }
-        return true;
+        else
+        {
+            return 1;
+        }
     }
 
     // set less than memory-memory
     // opcode 0101
-    bool sltm(std::string op1, std::string op2, std::string op3)
+    int sltm(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = (memory[M[op1]] < memory[M[op2]]) ? 1 : 0;
-        return true;
+        return 1;
     }
 
     // add immediate
     // opcode 0110
-    bool ai(std::string op1, std::string op2, std::string op3)
+    int ai(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] + std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // subtract immediate
     // opcode 0111
-    bool si(std::string op1, std::string op2, std::string op3)
+    int si(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] - std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // multiply immediate
     // opcode 1000
-    bool mi(std::string op1, std::string op2, std::string op3)
+    int mi(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = memory[M[op1]] * std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // divide immediate
     // opcode 1001
-    bool di(std::string op1, std::string op2, std::string op3)
+    int di(std::string op1, std::string op2, std::string op3)
     {
         if (std::stoi(op2) == 0)
         {
-            return false;
+            return 0;
         }
         memory[M[op3]] = memory[M[op1]] / std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // load constant immediate
     // opcode 1010
-    bool lci(std::string op1, std::string op2, std::string op3)
+    int lci(std::string op1, std::string op2, std::string op3)
     {
         memory[M[op3]] = std::stoi(op1) + std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // subtract constant immediate
     // opcode 1011
-    bool sci(std::string op1, std::string op2, std::string op3)
+    int sci(std::string op1, std::string op2, std::string op3)
     {
         // is this right?
         memory[M[op3]] = std::stoi(op1) + std::stoi(op2);
-        return true;
+        return 1;
     }
 
     // load using offset
     // opcode 1100
-    bool lo(std::string op1, std::string op2, std::string op3)
+    int lo(std::string op1, std::string op2, std::string op3)
     {
-
-        memory[memory[M[op3]]] = M[op1] + memory[M[op2]];
-        // or memory[M[op3]] = memory[M[op1]] + memory[M[op2]];
-        return true;
+        memory[M[op3]] = memory[memory[M[op1]] + memory[M[op2]]];
+        return 1;
     }
 
     // store using offset
     // opcode 1101
-    bool so(std::string op1, std::string op2, std::string op3)
+    int so(std::string op1, std::string op2, std::string op3)
     {
-        memory[memory[M[op1]] + memory[M[op1]]] = memory[M[op2]];
-        return true;
+        memory[memory[M[op1]] + memory[M[op2]]] = memory[M[op3]];
+        return 1;
     }
 
     // jump back
     // opcode 1110
-    bool jb(std::string op1, std::string op2, std::string op3)
+    int jb(std::string op1, std::string op2, std::string op3)
     {
-        pc -= std::stoi(op1) + std::stoi(op2) + std::stoi(op3);
-        return true;
+        return -(std::stoi(op1) + std::stoi(op2) + std::stoi(op3));
     }
 
     void printMemory()
@@ -248,11 +247,17 @@ public:
         }
     }
 
+    void printPC()
+    {
+        std::cout << "PC: " << pc << std::endl;
+    }
+
     // execute("0000","0000","0000","0000")
     // or
     // execute("am","M0","M0","M0")
     // or a combination of both?
     void execute(std::string opcode, std::string op1, std::string op2, std::string op3)
+    // void execute(uint8_t opcode, uint8_t op1, uint8_t op2, uint8_t op3)
     {
         // print opcode, op1, op2, op3
         std::cout << opcode << " " << op1 << " " << op2 << " " << op3 << std::endl;
@@ -260,8 +265,10 @@ public:
         int op = std::stoi(mappedOpCode, 0, 2);
         if (op >= 0 && op < functions.size())
         {
-            if (functions[op](op1, op2, op3))
+            int inc = functions[op](op1, op2, op3);
+            if (inc)
             {
+                pc += inc;
                 std::cout << "Operation successful." << std::endl;
             }
             else
@@ -274,29 +281,29 @@ public:
             std::cout << "Error: Invalid opcode." << std::endl;
         }
         // printMemory();
-        printInColumns(memory, 4);
+        printInColumns(memory, 8);
+        printPC();
     }
 };
 
 int main()
 {
 
-    const int memSize = 64;
+    const int memSize = 128;
     std::vector<int> mainMemory(memSize, 0);
 
     ALU alu(mainMemory, memSize);
 
-    // Example: Execute am with opcode 0
-    alu.execute("lci", "0", "8", "M0");
-    alu.execute("lci", "0", "8", "M1");
-    alu.execute("lci", "0", "7", "M2");
-    alu.execute("lci", "0", "11", "M3");
-    alu.execute("lci", "0", "10", "M4");
-    alu.execute("lci", "0", "4", "M5");
-    alu.execute("lci", "0", "1", "M6");
-    alu.execute("lci", "0", "0", "M7");
-    alu.execute("lci", "0", "1", "M8");
-    alu.execute("lci", "0", "1", "M9");
+    alu.execute("lci", "0", "8", "M0");  // min
+    alu.execute("lci", "0", "8", "M1");  // max
+    alu.execute("lci", "0", "7", "M2");  // 2
+    alu.execute("lci", "0", "11", "M3"); // 3
+    alu.execute("lci", "0", "10", "M4"); // 4
+    alu.execute("lci", "0", "4", "M5");  // size
+    alu.execute("lci", "0", "1", "M6");  // idx
+    alu.execute("lci", "0", "0", "M7");  // logic compare 1
+    alu.execute("lci", "0", "1", "M8");  // logic compare 0
+    alu.execute("lci", "0", "1", "M9");  // base address
 
     // am m9 m6 m15
     alu.execute("am", "M9", "M6", "M15"); // am operation (add mem-mem)
