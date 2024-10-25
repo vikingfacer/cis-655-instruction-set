61f172264c7f380dd52cab94837e334969aa6a86
ahamburglar <imchangliuu@gmail.com>
2024-10-24 23:13:24 -0700

    parser implementation

---
+++ parser/parser.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "parser.h"
+
+std::string delimiter = ",";
+
+std::vector<std::string> Parser::parse(std::ifstream &in) {
+    while(!in.eof()) {
+        std::string token;
+        std::vector<std::string> tokens;
+        while (!std::getline(in, token, ',').eof()) {
+            tokens.push_back(token);
+        }
+        
+        if (tokens.size() == 0)
+            continue;
+
+        // pass tokens to decoder 
+    }
+}
